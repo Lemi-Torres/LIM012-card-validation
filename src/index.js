c91cc949e483062e68643fb0ba8622a970ad9175
Lemi-Torres <lmt.2020v@gmail.com>
2020-02-20 11:16:34 -0500

    testeo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import validator from './validator.js';
+const cardNumberInput = document.getElementById('card-number-input');
+const cardNumberOutput = document.getElementById('card-number-output');
+cardNumberInput.addEventListener('input',(cardNumberOutput));
+const result = document.getElementById('result');
+const maskifynumber = document.getElementById('maskifynumber');
 
-console.log(validator);
+document.getElementById('segundo').style.display="none";
+document.getElementById('tercero').style.display="none";
+document.getElementById('result').style.display="none";
+
+const btn1=document.getElementById('btn1');
+btn1.addEventListener('click',() => {
+    document.getElementById('primero').style.display="none";
+    document.getElementById('segundo').style.display="block";
+    
+});
+
+const btn2=document.getElementById('btn2');
+btn2.addEventListener('click',() => {
+    document.getElementById('segundo').style.display="none";
+    document.getElementById('tercero').style.display="block";
+  
+});
+
+const btn3=document.getElementById('btn3');
+btn3.addEventListener('click',() => {
+    result.innerHTML = 
+    `
+      <p ${validator.isValid(cardNumberInput.value) === true ? 'valid-text">Tarjeta válida' : 'invalid-text">Tarjeta inválida'}</p>     
+      `
+      document.getElementById('result').style.display="block";
+      maskifynumber.innerHTML=validator.maskify(cardNumberInput.value)
+
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,55 @@
-const validator = {
-  // ...
-};
+//const validator es un objeto
+const validator = { 
 
-export default validator;
+  //Declarar dentro del metodo isValid una variable llamada number la cuál va almacenar los números ingresados.
+   isValid:(creditCardNumber)=> {
+ 
+     //Declarar un const llamado arrayReverse el cual almacenara los números en orden inverso para aplicar el algoritmo de Luhn.
+     const arrayReverse = creditCardNumber.toString().split('').reverse();
+     //Declarar un const llamado arraySum para almacenar la suma de los digitos.
+     const arraySum = [];
+ 
+     //Aplicando el algoritmo de Luhn:
+     if (creditCardNumber.length === 0) {
+       alert('ERROR! No ingresó su número de tarjeta');
+       return false;
+     }
+     //El método forEach() ejecutara la función indicada una vez por cada elemento del array,en orden.
+     arrayReverse.forEach((num, i) => {
+       
+       if(i % 2 !== 0) {
+         let numPar = num * 2;
+         if(numPar >= 10) {          
+           arraySum.push(
+             numPar.toString().split('').reduce((a, b) => parseInt(a) + parseInt(b))
+           );
+         } else {
+           arraySum.push(numPar);
+         }
+       } else {
+         arraySum.push(parseInt(num));
+       }
+     });
+ 
+     
+     
+     const totalSum = arraySum.reduce((a, b) => a + b);
+     if(totalSum % 10 === 0) {
+       return true
+     } else {   
+       return false
+     }  
+ 
+   },
+ 
+   maskify:(creditCardNumber)=> {
+     const cardNumber = creditCardNumber.split('');
+     for(let i=0; i<cardNumber.length -4;i++){
+       cardNumber[i]='#';
+     }
+     return cardNumber.join('')
+   }
+  
+  
+ };
+ export default validator;
